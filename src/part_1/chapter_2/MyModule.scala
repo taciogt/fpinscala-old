a6ca605dd406ff533d321d5bfdecc16fff1e27a5
Tácio Tavares <taciogt@gmail.com>
2017-06-18 19:58:19 -0300

    first example of higher order function

--- src/part_1/chapter_2/MyModule.scala
+++ src/part_1/chapter_2/MyModule.scala
@@ -22,6 +22,22 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
-  def main (args: Array[String]): Unit =
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
+  def main (args: Array[String]): Unit = {
     println(formatAbs(-42))
+    println(formatFactorial(7))
+
+    println(formatResult("absolute value", -43, abs))
+  }
+
+
 }
